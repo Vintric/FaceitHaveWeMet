ec7f40f75fc8ab298e68ba38df797bb5feda3d7d
angeloalfanoc <alfanoangeloc@gmail.com>
2019-04-24 16:17:53 +0200

    latest

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ let endDataPrev = 0;
 let setDate = false;
 let getTime;
 let faceiturl;
-const currentVersion= "0.40";
+const currentVersion= "0.41";
 
 let demoStorage = []
 let matchesStorage = []
