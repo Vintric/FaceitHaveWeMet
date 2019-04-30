e42345b4cb2f72cf27fa33058dae305787afe08b
angeloalfanoc <alfanoangeloc@gmail.com>
2019-04-30 15:36:25 +0200

    change favicon

--- assets/js/app.js
+++ assets/js/app.js
@@ -330,7 +330,7 @@ $(function() {
     
       var $form = $(this);
       $.post($form.attr("action"), $form.serialize()).then(function() {
-
+        alert('test')
       });
     });
     $("#searchButton").click(function(e) {
